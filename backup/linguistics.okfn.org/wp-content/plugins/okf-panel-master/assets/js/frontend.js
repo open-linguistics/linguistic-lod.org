220062ce545d1658c5b537e4a031411fb95f14b7
Christian Chiarcos <christian.chiarcos@web.de>
2020-01-20 05:15:38 +0100

    backup of linguistics.okfn.org

---
+++ backup/linguistics.okfn.org/wp-content/plugins/okf-panel-master/assets/js/frontend.js
@@ -0,0 +1,2 @@
+jQuery(document).ready(function($){if(typeof($.fn.collapse)==='undefined'){$.getScript('https://assets.okfn.org/themes/okfn/collapse.min.js');}
+$.getScript('https://assets.okfn.org/themes/okfn/okf-panel.js');$("body").addClass("okf-panel");});
